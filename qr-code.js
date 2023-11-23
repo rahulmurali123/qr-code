a31c14f45319a0753db6dd4e61f4ae4e837e535d
rahulmurali123 <125792213+rahulmurali123@users.noreply.github.com>
2023-11-23 22:58:57 +0530

    Add files via upload

---
+++ qr-code.js
@@ -0,0 +1,20 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+    message: "Type in your url", 
+    name: "URL"},
+])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_image.png"));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+    }
+  });
